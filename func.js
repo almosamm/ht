546f97d798eb448c48ee53a2a4a8523bef95e826
almosamm <48527536+almosamm@users.noreply.github.com>
2023-04-23 13:18:24 +0000

    Update func.js

--- func.js
+++ func.js
@@ -3,5 +3,12 @@
     for(var i = 0; i < divsToHide.length; i++){
         divsToHide[i].style.visibility = "hidden"; // or
         divsToHide[i].style.display = "none"; // depending on what you're doing
-    }
+    };
+
+
+document.querySelectorAll('.blogPts').forEach(function(el) {
+   el.style.display = 'none';
+});
 </script>
+
+
